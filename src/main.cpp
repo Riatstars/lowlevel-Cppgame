564bf7839ce79a8d80a3b2701953f2309b56ba64
RiatStars <riatstars@gmail.com>
2026-04-13 22:22:17 +0900

    add Raylib

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <raylib.h>
 
 int main()
 {
 	std::cout << "Hello\n";
 
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+	InitWindow(800, 450, "my first game");
+
+	while (!WindowShouldClose()) {
+		
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+		DrawText("Congradulation! You have created your first window",190,200,20,RED);
+
+		EndDrawing();
+
+	}
+	CloseWindow();
+
 	return 0;
-}
+}	
