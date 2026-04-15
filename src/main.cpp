ec4da634e0d8d9a0eccc14af69ebc7476bf31fc4
RiatStars <riatstars@gmail.com>
2026-04-15 21:03:03 +0900

    add imgui and rlimgui

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <raylib.h>
 
+#include <imgui.h>
+#include <rlImGui.h>
+
 int main()
 {
 	std::cout << "Hello\n";
@@ -8,15 +11,28 @@ int main()
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 	InitWindow(800, 450, "my first game");
 
+	rlImGuiSetup(true);
+
 	while (!WindowShouldClose()) {
 		
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
+		rlImGuiBegin();
 		DrawText("Congradulation! You have created your first window",190,200,20,RED);
 
+		ImGui::Begin("TEST");
+		ImGui::Text("hello");
+		ImGui::Button("button");
+		ImGui::ShowDemoWindow();
+
+		ImGui::End();
+
+		rlImGuiEnd();
 		EndDrawing();
 
 	}
+
+	rlImGuiShutdown();
 	CloseWindow();
 
 	return 0;
